6c53b7e3a394b59c2f80a7de913ca719127fe120
Rodrigopca42 <rodrigopca426281@gmail.com>
2026-09-26 08:17:14 -0300

    desenvolvimento do doc home_page.py

---
+++ pages/home_page.py
@@ -0,0 +1,43 @@
+from selenium.webdriver.common.by import By
+
+class HomePage:
+
+    # ========================================================
+    # LOCATORS
+    # ========================================================
+
+    GREY_JACKET = (By.ID, "product-1")
+    NOIR_JACKET = (By.ID, "product-2")
+    STRIPED_TOP = (By.ID, "product-3")
+
+    # ========================================================
+    # CONSTRUTOR
+    # ========================================================
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    # ========================================================
+    # AÇÕES
+    # ========================================================
+
+    def acessar_grey_jacket(self):
+        produto = self.driver.find.element(
+            *self.GREY_JACKET
+        )
+
+        produto.click()
+
+    def acessar_noir_jacket(self):
+        produto = self.driver.find.element(
+            *self.NOIR_JACKET
+        )    
+
+        produto.click()
+
+    def acessar_stirped_top(self):
+        produto = self.driver.find.element(
+            *self.STRIPED_TOP
+        )
+
+        produto.click()
